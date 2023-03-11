ebb670998031cf8a73f17cf064e05d14e427653d
4pers0n <zhw2513953778@outlook.com>
2023-03-11 15:15:07 -0800

    Add connect to server script

---
+++ PersonalBubbleUnityProject/Assets/_Scripts/Photon/ConnectToServer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class ConnectToServer : MonoBehaviourPunCallbacks
+{
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        Debug.Log("successfully joined the lobby");
+    }
+}
